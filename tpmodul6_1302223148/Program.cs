73a422812b89de90102fee07b53dca6dc5a7fc0b
nasyaarnm <nasyaarnm@student.telkomuniversity.ac.id>
2024-03-24 23:14:52 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302223148/Program.cs
+++ tpmodul6_1302223148/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using tpmodul6_1302223148;
+
+
+public class Program()
+{
+    public static void Main (String[] args)
+    {
+        SayaTubeVideo nama = new SayaTubeVideo("Tutorial Design by Contract-{NASYA_KIRANA_MARENDRA}");
+        nama.IncreasePlayCount(1000000);
+        nama.printVideoDetails();
+
+    }
+}
---
+++ tpmodul6_1302223148/SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302223148
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+
+        public void printVideoDetails()
+        {
+            Console.WriteLine("ID: "+ id);
+            Console.WriteLine("Title: "+ title);
+            Console.WriteLine("Play COunt: " + playCount);   
+        }
+
+    }
+}
